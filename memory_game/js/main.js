ab873a6f499a7c40bdfc64aedc1b34e49e6e9f71
wongsitu <wongsitu@ksu.edu>
2018-06-17 14:17:02 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,24 @@
-var cards = ["queen", "queen", "king","king"];
+var cards = [{
+rank:"queen",
+suit:"hearts",
+cardImage:"images/queen-of-hearts.png"
+},
+{ 
+rank:"queen",
+suit:"diamonds",
+cardImage:"images/queen-of-diamonds.png"
+},
+{ 
+rank:"king",
+suit:"hearts",
+cardImage:"images/king-of-hearts.png"
+},
+{
+rank:"king",
+suit:"diamonds",
+cardImage:"images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
@@ -14,9 +34,12 @@ var flipCard = function(cardId){
 	if(cardsInPlay.length === 2){
 		checkForMatch();
 	}
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
+
 
 flipCard(0);
 flipCard(2);
